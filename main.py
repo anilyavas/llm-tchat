4ef6ae0e2cf6156ea5a02a2d49bc962153074e8d
anilyavas <anil.yavas99@gmail.com>
2024-07-16 22:00:01 +0300

    chatbot starter

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 
+load_dotenv()
+
+chat = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+human_message_template = HumanMessagePromptTemplate.from_template("{content}")
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"], messages=[HumanMessagePromptTemplate.from_template("")]
+    input_variables=["content"],
+    messages=[human_message_template],
 )
+
+chain = LLMChain(llm=chat, prompt=prompt)
+
 while True:
     content = input(">> ")
 
-    print(f"You entered: {content}")
+    result = chain.run({"content": content})
+
+    print(result)
