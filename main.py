e99f9b70b06b4728b6ddc28a4adfdef19dd68803
anilyavas <anil.yavas99@gmail.com>
2024-07-20 17:59:33 +0300

    chatbot basic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
@@ -12,11 +12,12 @@ from langchain.prompts import (
 
 load_dotenv()
 
-chat = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+chat = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), verbose=True)
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
     memory_key="messages",
     return_messages=True,
+    llm=chat,
 )
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
@@ -25,7 +26,7 @@ prompt = ChatPromptTemplate(
         HumanMessagePromptTemplate.from_template("{content}"),
     ],
 )
-chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory, verbose=True)
 
 while True:
     content = input(">> ")
